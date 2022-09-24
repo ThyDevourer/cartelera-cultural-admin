d41c5db847a98dfd92f2bf7ca60a2dca48abfb66
ThyDevourer <pedro@ivanpartida.xyz>
2022-09-24 10:57:43 -0700

    feat(login): persist user session

--- src/hooks/useSessionStore.ts
+++ src/hooks/useSessionStore.ts
@@ -1,6 +1,6 @@
 import jwtDecode from 'jwt-decode'
 import create from 'zustand'
-import { devtools } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import { login, signup, verify } from '../services/session'
 import { IUser, SignupPayload } from '../types/interfaces'
 
@@ -35,53 +35,59 @@ const initialUserState: UserState = {
 }
 
 export const useSessionStore = create<SessionState>()(
-  devtools((set, get) => ({
-    isLoggedIn: false,
-    user: initialUserState,
-    error: null,
-    setError: (err) => {
-      if (err === null) {
-        set(() => ({ error: null }))
-      } else {
-        set(() => ({ error: err.message }))
-      }
-    },
-    login: async (username: string, password: string) => {
-      try {
-        const { token } = await login({ username, password })
-        const decodedToken = jwtDecode<IUser>(token)
-        const user = { ...decodedToken, token }
-        set(() => ({ user, isLoggedIn: true }))
-      } catch (e: any) {
-        set(() => ({ error: e.message }))
-        throw e
-      }
-    },
-    logout: () => set(() => ({
-      isLoggedIn: false,
-      user: initialUserState
-    })),
-    signup: async (payload) => {
-      try {
-        const user = await signup(payload)
-        set(() => ({
-          user: {
-            ...user,
-            token: get().user.token
-          },
-          isLoggedIn: true
-        }))
-      } catch (e: any) {
-        set(() => ({ error: e.message }))
-      }
-    },
-    verify: async (code) => {
-      try {
-        const user = await verify({ code, userId: get().user._id })
-        set({ user: { ...user, token: get().user.token, verified: true } })
-      } catch (e: any) {
-        set({ error: e.message })
-      }
-    }
-  }), { name: 'Session' })
+  devtools(
+    persist(
+      (set, get) => ({
+        isLoggedIn: false,
+        user: initialUserState,
+        error: null,
+        setError: (err) => {
+          if (err === null) {
+            set(() => ({ error: null }))
+          } else {
+            set(() => ({ error: err.message }))
+          }
+        },
+        login: async (username: string, password: string) => {
+          try {
+            const { token } = await login({ username, password })
+            const decodedToken = jwtDecode<IUser>(token)
+            const user = { ...decodedToken, token }
+            set(() => ({ user, isLoggedIn: true }))
+          } catch (e: any) {
+            set(() => ({ error: e.message }))
+            throw e
+          }
+        },
+        logout: () => set(() => ({
+          isLoggedIn: false,
+          user: initialUserState
+        })),
+        signup: async (payload) => {
+          try {
+            const user = await signup(payload)
+            set(() => ({
+              user: {
+                ...user,
+                token: get().user.token
+              },
+              isLoggedIn: true
+            }))
+          } catch (e: any) {
+            set(() => ({ error: e.message }))
+          }
+        },
+        verify: async (code) => {
+          try {
+            const user = await verify({ code, userId: get().user._id })
+            set({ user: { ...user, token: get().user.token, verified: true } })
+          } catch (e: any) {
+            set({ error: e.message })
+          }
+        }
+      }),
+      { name: 'session' }
+    ),
+    { name: 'session' }
+  )
 )
