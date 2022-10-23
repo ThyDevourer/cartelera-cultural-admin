ecc15a04180fd7d862c3a6683be6e506af2edad3
ThyDevourer <pedro@ivanpartida.xyz>
2022-10-23 12:02:42 -0700

    feat: add meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HeadProvider } from 'react-head'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HeadProvider>
+      <App />
+    </HeadProvider>
   </React.StrictMode>
 )
